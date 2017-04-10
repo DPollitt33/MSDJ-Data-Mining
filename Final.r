36aa801a8a00087b808b8beb9a6e2905f35c39f5
Dakota E. Pollitt <pollit33@students.rowan.edu>
2017-04-10 01:28:04 -0400

    V 0.2 Made a clean data set

--- Final.r
+++ Final.r
@@ -10,28 +10,8 @@ rm(list = ls(all = TRUE))
 # Load data
 data <- read.csv('DfTRoadSafety_Accidents_2012.csv', header=TRUE)
 
-# DATA PROCESSING
+# DATA PREPROCESSING
 
-  # Factorize fields
-  data$Police_Force <- factor(data$Police_Force)
-  data$Accident_Severity <- factor(data$Accident_Severity)
-  data$Day_of_Week <- factor(data$Day_of_Week)
-  data$Local_Authority_.District. <- factor(data$Local_Authority_.District.)
-  data$X1st_Road_Class <- factor(data$X1st_Road_Class)
-  data$Road_Type <- factor(data$Road_Type)
-  data$Junction_Detail <- factor(data$Junction_Detail)
-  data$Junction_Control <- factor(data$Junction_Control)
-  data$X2nd_Road_Class <- factor(data$X2nd_Road_Class)
-  data$Pedestrian_Crossing.Human_Control <- factor(data$Pedestrian_Crossing.Human_Control)
-  data$Pedestrian_Crossing.Physical_Facilities <- factor(data$Pedestrian_Crossing.Physical_Facilities)
-  data$Light_Conditions <- factor(data$Light_Conditions)
-  data$Weather_Conditions <- factor(data$Weather_Conditions)
-  data$Road_Surface_Conditions <- factor(data$Road_Surface_Conditions)
-  data$Special_Conditions_at_Site <- factor(data$Special_Conditions_at_Site)
-  data$Carriageway_Hazards <- factor(data$Carriageway_Hazards)
-  data$Urban_or_Rural_Area <- factor(data$Urban_or_Rural_Area)
-  data$Did_Police_Officer_Attend_Scene_of_Accident <- factor(data$Did_Police_Officer_Attend_Scene_of_Accident)
-  
   # Rename verbose fields
   names(data)[names(data) == 'Latitude'] <- "Lat"
   names(data)[names(data) == 'Longitude'] <- "Long"
@@ -47,17 +27,39 @@ data <- read.csv('DfTRoadSafety_Accidents_2012.csv', header=TRUE)
   names(data)[names(data) == 'Road_Surface_Conditions'] <- "Surface"
   names(data)[names(data) == 'Special_Conditions_at_Site'] <- "Special"
   names(data)[names(data) == 'Did_Police_Officer_Attend_Scene_of_Accident'] <- "Police_attendance"
+  names(data)[names(data) == 'Accident_Index'] <- "Index"
+  names(data)[names(data) == 'Location_Easting_OSGR'] <- "E_OSGR"
+  names(data)[names(data) == 'Location_Northing_OSGR'] <- "N_OSGR"
+  names(data)[names(data) == 'Local_Authority_.District.'] <- "LA_District"
+  names(data)[names(data) == 'Local_Authority_.Highway.'] <- "LA_Highway"
+  names(data)[names(data) == 'Day_of_Week'] <- "Day"
+  names(data)[names(data) == 'LSOA_of_Accident_Location'] <- "LSOA"
   
-  # Factors with missing values, as given by the guide
-  # 1st Road 6-Unclassified?
-  # Road Type -1
-  # Junction Detail -1
-  # Junction Control -1
-  # 2nd Road 6-Unclassifed?
-  # Ped Cross Human -1
-  # Ped Cross Physical -1
-  # Light Conditions -1
-  # Weather -1
-  # Road Surface -1
-  # Special Conditions -1
-  # Carriageway Hazards -1
+
+  # Factorize fields
+  data$Police_Force <- factor(data$Police_Force)
+  data$Severity <- factor(data$Severity)
+  data$Day <- factor(data$Day)
+  data$LA_District <- factor(data$LA_District)
+  data$First_road_class <- factor(data$First_road_class)
+  data$Road_Type <- factor(data$Road_Type)
+  data$Junction_Detail <- factor(data$Junction_Detail)
+  data$Junction_Control <- factor(data$Junction_Control)
+  data$Second_road_class <- factor(data$Second_road_class)
+  data$Ped_xing_human <- factor(data$Ped_xing_human)
+  data$Ped_xing_human <- factor(data$Ped_xing_human)
+  data$Light <- factor(data$Light)
+  data$Weather <- factor(data$Weather)
+  data$Surface <- factor(data$Surface)
+  data$Special <- factor(data$Special)
+  data$Carriageway_Hazards <- factor(data$Carriageway_Hazards)
+  data$Urban_or_Rural_Area <- factor(data$Urban_or_Rural_Area)
+  data$Police_attendance <- factor(data$Police_attendance)
+  
+  # Clear Missing values
+  cleanData <- data[!(is.na(data$LSOA) | data$LSOA==""),]
+  
+  # Clear data outside range
+  cleanData <- subset(cleanData, Junction_Control != -1)
+  cleanData <- subset(cleanData, Second_road_class != -1)
+  cleanData <- subset(cleanData, Surface != -1)
